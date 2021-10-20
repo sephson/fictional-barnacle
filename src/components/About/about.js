bb014f7ea180b8de533e8737ce1c99058ef7949c
sephson <disutj@gmail.com>
2021-10-20 18:26:59 +0100

    about correction

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -13,7 +13,7 @@ const about = () => {
         but my interest in coding started in 2020 because I wanted to build my
         own games and software. I started learning how to code in November,
         2020. I have hands on experience developing websites, full-stack web
-        applications and APIs. Currently, I'm learning Java and also improving
+        applications and APIs. Currently, I'm learning Python and also improving
         my problem solving skills by learning Data Structures and Algorithm.
       </p>
     </div>
